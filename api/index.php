ed9e455f5c84baa905ee9890ab2721f45a6baa57
Marvin M. Ramos <115031416+Crispkreme@users.noreply.github.com>
2026-08-10 18:26:05 +0800

    fixed files

--- api/index.php
+++ api/index.php
@@ -1,33 +1,17 @@
 <?php
 
-use Illuminate\Contracts\Http\Kernel;
+use Illuminate\Foundation\Application;
 use Illuminate\Http\Request;
 
-try {
-    require __DIR__ . '/../vendor/autoload.php';
+define('LARAVEL_START', microtime(true));
 
-    $app = require __DIR__ . '/../bootstrap/app.php';
-
-    $kernel = $app->make(Kernel::class);
-
-    $request = Request::capture();
-
-    $response = $kernel->handle($request);
-
-    $response->send();
-
-    $kernel->terminate($request, $response);
-
-} catch (Throwable $e) {
-    http_response_code(500);
-
-    header('Content-Type: application/json');
-
-    echo json_encode([
-        'success' => false,
-        'message' => $e->getMessage(),
-        'file' => $e->getFile(),
-        'line' => $e->getLine(),
-        'trace' => $e->getTrace(),
-    ], JSON_PRETTY_PRINT);
+if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
+    require $maintenance;
 }
+
+require __DIR__.'/../vendor/autoload.php';
+
+/** @var Application $app */
+$app = require_once __DIR__.'/../bootstrap/app.php';
+
+$app->handleRequest(Request::capture());
